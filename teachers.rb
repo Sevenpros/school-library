55c18d7359e5fb14c2bc9a83cd2162bb5b084e94
Sevenpros <sethbisoso@gmail.com>
2022-10-05 11:43:49 +0200

    Add classes

---
+++ teachers.rb
@@ -0,0 +1,10 @@
+require './person'
+
+class Teacher < Person 
+    def initialize(specialization)
+        @specialization = specialization
+    end
+    def can_use_services
+        true
+    end
+end
